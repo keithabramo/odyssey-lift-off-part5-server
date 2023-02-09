6af3466a6d8dad1432e1670c2e7add7039ba3a75
Keith Abramo <Abramo.Keith@mayo.edu>
2023-02-09 07:32:37 -0600

    Typo

--- src/schema.js
+++ src/schema.js
@@ -36,7 +36,7 @@ const typeDefs = gql`
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
     "The track's approximate length to complete, in minutes"
-    length: Int @depricated(reason: "Use durationInSeconds")
+    length: Int @deprecated(reason: "Use durationInSeconds")
     "The module's video duration, in seconds"
     durationInSeconds: Int
     "The number of modules this track contains"
